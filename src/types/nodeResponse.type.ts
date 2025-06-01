663f1ee524dc06cb98e71edba511008eff448cf1
Menhythien <dinhthienmenh1505@gmail.com>
2025-06-01 09:44:38 +0700

    feat(type): utility type for node response

---
+++ src/types/nodeResponse.type.ts
@@ -0,0 +1,44 @@
+export interface CreateBattleResponse {
+    jsonrpc: string;
+    id: number;
+    result: {
+        digest: string;
+        objectChanges: ObjectChange[];
+        confirmedLocalExecution: boolean;
+    }
+}
+
+export type ObjectChange = MutatedObject | CreatedObject | DeletedObject;
+
+export interface BaseObjectChange {
+    type: 'mutated' | 'created' | 'deleted';
+    sender: string;
+    owner: {
+        AddressOwner?: string;
+        ObjectOwner?: string;
+        Shared?: {
+            initial_shared_version: string;
+        };
+        Immutable?: null;
+    };
+    objectType: string;
+    objectId: string;
+    version: string;
+    digest: string;
+}
+
+export interface MutatedObject extends BaseObjectChange {
+    type: 'mutated';
+    previousVersion: string;
+}
+
+export interface CreatedObject extends BaseObjectChange {
+    type: 'created';
+}
+
+export interface DeletedObject extends BaseObjectChange {
+    type: 'deleted';
+    previousVersion: string;
+}
+
+// Utility types để truy xuất thông tin từ response dễ dàng hơn
